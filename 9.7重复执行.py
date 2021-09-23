9718ef0d76d0975aaa3c22c9cd456c9502b67410
amazing-yx <79432263+amazing-yx@users.noreply.github.com>
2021-09-23 12:49:12 +0800

    Add files via upload

---
+++ 9.7重复执行.py
@@ -0,0 +1,13 @@
+#   -*- coding = utf-8 -*-
+#   @time : 2021/9/7 13:16
+#   @ File : 9.7重复执行.py
+#   @Software: PyCharm
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# encoding: utf-8
+import os
+
+if __name__ == '__main__':
+    for i in range(1):
+        # cmd = 'python D:\学习笔记\ll_code\10node cheshi.py'  # 必须转义'\'
+        os.system('python 10node1.py')
